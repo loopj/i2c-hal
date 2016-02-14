4f84a2d43c605053829aa19f7962542c5016e353
James Smith <james@loopj.com>
2016-02-13 19:08:46 -0800

    Example for all other devices too

--- examples/wiringpi/src/main.cpp
+++ examples/wiringpi/src/main.cpp
@@ -17,6 +17,36 @@ int main() {
             printf("Acceleration (m/s^2)  %+7.3f, %+7.3f, %+7.3f\n", a.x, a.y, a.z);
         }
 
+        Barometer *barometer = Sensors::getBarometer();
+        if(barometer) {
+            float p = barometer->getPressure();
+            printf("Pressure (hPa)        %+7.3f\n", p);
+
+            float a = barometer->getAltitude();
+            printf("Altitude (m)          %+7.3f\n", a);
+        }
+
+        Gyroscope *gyroscope = Sensors::getGyroscope();
+        if(gyroscope) {
+            Vector3 g = gyroscope->getRotation();
+            printf("Rotation (rad/s)      %+7.3f, %+7.3f, %+7.3f\n", g.x, g.y, g.z);
+        }
+
+        Magnetometer *magnetometer = Sensors::getMagnetometer();
+        if(magnetometer) {
+            Vector3 m = magnetometer->getMagneticField();
+            printf("Magnetic Field (uT)   %+7.3f, %+7.3f, %+7.3f\n", m.x, m.y, m.z);
+
+            float azimuth = magnetometer->getAzimuth();
+            printf("Azimuth (deg)         %+7.3f\n", azimuth);
+        }
+
+        Thermometer *thermometer = Sensors::getThermometer();
+        if(thermometer) {
+            float t = thermometer->getTemperature();
+            printf("Temperature (C)       %+7.3f\n", t);
+        }
+
         delay(50);
     }
 }
